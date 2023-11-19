37702e70f1e044948a4870f5544b0748a12a55f1
Dev Batch Migration <platform-batch@fintechco.example>
2023-11-19 09:07:00 +0000

    ledger: add settle() for multi-leg statements; leap-year day count in interest

--- app/ledger/rounding.py
+++ app/ledger/rounding.py
@@ -21,9 +21,20 @@ def allocate(total, weights):
     return base
 
 
-def apply_daily_interest(balance, rate, days):
+def apply_daily_interest(balance, rate, days, year=2023):
     r = rate / 10000.0
+    d = 366.0 if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0 else 365.0
     b = balance
     for _ in range(days):
-        b = b + math.floor(b * r / 365.0)
+        b = b + math.floor(b * r / d)
     return b
+
+
+def settle(legs):
+    # legs: list of (weight, balance, rate, days). returns per-leg settled cents.
+    total = sum(bal for _, bal, _, _ in legs)
+    parts = allocate(total, [w for w, _, _, _ in legs])
+    out = []
+    for (_, _bal, rate, days), p in zip(legs, parts, strict=False):
+        out.append(apply_daily_interest(p, rate, days))
+    return out
